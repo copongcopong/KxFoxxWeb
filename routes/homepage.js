284801d0a2a89884adbf3be0ed2ddd0798a50566
Earl Celis <copongcopong@gmail.com>
2017-05-12 13:50:13 +0800

    created new page; set posts from blog collection

--- routes/homepage.js
+++ routes/homepage.js
@@ -6,10 +6,11 @@
 const createRouter = require('@arangodb/foxx/router');
 const router = createRouter();
 module.exports = router;
+const $blog = module.context.collection('blog');
 
 router.get('/', function (req, res) {
   try {
-    res.view('homepage-index', {text: " [Text from data set in homepage route]"}, 'main');
+    res.view('homepage-blogposts', {posts: $blog.byExample({}).toArray() }, 'main');
   } catch (e) {
     //on error, use local file instead
     res.view('homepage/index.html', {text: "Worldy"}, 'main');
